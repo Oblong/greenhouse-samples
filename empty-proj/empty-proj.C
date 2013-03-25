24ab1f058a68ae60d028b55953a63f7bc4f7a2a7
Brandon Harvey (bbook) <brandon@oblong.com>
2013-03-25 11:31:51 -0700

    Added helloworld sample and 'empty' sample
    - These are the simplest things that can possibly work.
    - Greenhouse SDK must already be installed.
    - Each sample includes a vanilla Makefile as well as an XCode project file.
    - To build (e.g. helloworld) at the command line:
        cd helloworld
        make
        ./helloworld
    - To build with XCode:
        open helloworld.xcodeproj
        *press Run button*
    - The empty sample simply provides a place to paste in code from samples

---
+++ empty-proj/empty-proj.C
@@ -0,0 +1,14 @@
+
+/* (c)  oblong industries */
+
+// ###################################################################
+// Replace these contents (perhaps with sample code from the reference
+// section of the Greenhouse website.)  
+// Type 'make' at the command line, or click "Run" in XCode.
+
+#include "Greenhouse.h"
+
+void Setup ()
+{ }
+
+// ###################################################################
---
+++ helloworld/helloworld.C
@@ -0,0 +1,11 @@
+
+/* (c)  oblong industries */
+
+
+#include "Greenhouse.h"
+
+
+void Setup ()
+{ Text *t = new Text ("Hello, world.");      // Make a new Text object &
+  t -> SlapOnFeld ();         // place it at the center of the main feld
+}
