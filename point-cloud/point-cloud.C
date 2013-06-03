51fc61dc3e9e4a6f65fef3db1fcc263af46698d2
Brandon Harvey (bbook) <brandon@oblong.com>
2013-06-03 14:43:55 -0700

    added comment about finding assets to point-cloud.C

--- point-cloud/point-cloud.C
+++ point-cloud/point-cloud.C
@@ -1,6 +1,25 @@
 
 #include <Greenhouse.h>
 
+/**
+ To ensure the image resources are found when running this sample, you will
+ need to do the following:
+ 
+ If running in Xcode:
+ - set your working directory to this sample's share directory:
+ 
+ - Go to Product -> Scheme -> Edit Scheme
+ - Under “Options”, select “Use custom working directory” and point it
+ to the share folder located in the same location as this .C file
+ 
+ If running via terminal:
+ - add the local share/ folder to your OB_SHARE_PATH by running the
+ following command in your terminal (with your correct path subbed in):
+ 
+ $ export OB_SHARE_PATH=/path/to/point-cloud/:$OB_SHARE_PATH
+ */
+
+
 class Brain  :  public DataTable,
                 public PointCloud
 {
