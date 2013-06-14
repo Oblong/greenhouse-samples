05e2509571b3713a4abea2765b1b4a113dd0565b
Brandon Harvey (bbook) <brandon@oblong.com>
2013-06-14 15:52:17 -0700

    put point-cloud resources into a share/ dir, matching all others

--- point-cloud/point-cloud.C
+++ point-cloud/point-cloud.C
@@ -4,19 +4,19 @@
 /**
  To ensure the image resources are found when running this sample, you will
  need to do the following:
- 
+
  If running in Xcode:
  - set your working directory to this sample's share directory:
- 
+
  - Go to Product -> Scheme -> Edit Scheme
  - Under “Options”, select “Use custom working directory” and point it
  to the share folder located in the same location as this .C file
- 
+
  If running via terminal:
  - add the local share/ folder to your OB_SHARE_PATH by running the
  following command in your terminal (with your correct path subbed in):
- 
- $ export OB_SHARE_PATH=/path/to/point-cloud/:$OB_SHARE_PATH
+
+ $ export OB_SHARE_PATH=/path/to/point-cloud/share:$OB_SHARE_PATH
  */
 
 
@@ -46,8 +46,8 @@ public:
 };
 
 void Setup ()
-{ Brain *brain = new Brain ("brain.txt");
-  brain -> LoadShaders ("brain.vert", "brain.frag");
+{ Brain *brain = new Brain ("data/brain.txt");
+  brain -> LoadShaders ("shaders/brain.vert", "shaders/brain.frag");
   Vect center = brain -> Center ();
   brain -> SetTranslation (Feld () -> Loc () - Vect(center.x, center.y, 1000.0));
   brain -> RotationAnimateSine (center + Vect (0, 1, 0), 180.0, 10.0);
